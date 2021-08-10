379deb00141b020e2dad6b03027ebbc3a5e46fd4
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-08-10 13:21:51 +0300

    сборка

--- src/components/Container/Container.jsx
+++
@@ -1,7 +0,0 @@
-import s from 'components/Container/Container.module.css';
-
-const Container = ({ children }) => {
-  return <section className={s.container}>{children}</section>;
-};
-
-export default Container;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store, { persistor } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import './index.css';
-import App from './App';
+
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
